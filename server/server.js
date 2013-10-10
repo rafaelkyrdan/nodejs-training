459da8d0e310a7d9e83ed9db15d5442ace5abdb1
Rafael <rkyrdan@cogniance.com>
2013-10-10 15:08:31 +0300

    next step

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 var http = require("http"),
-    server = new http.Server();
+    server = new http.Server(),
+    url = require('url'),
+    urlParsed;
 
 server.listen(1337, "127.0.0.1");
 server.on("request", function (req, res) {
-    res.end("Hello world!");
+
+    console.log(req.headers);
+
+
+    urlParsed = url.parse(req.url, true);
+    console.log(urlParsed);
+
+    if (urlParsed.pathname === "/echo" && urlParsed.query.message) {
+        res.end(urlParsed.query.message);
+    } else {
+        res.statusCode = 404;
+        res.end("Page not found");
+    }
+    
 });
