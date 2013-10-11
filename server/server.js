19bd474b09d61078cb8c3133604d4177345a3ad0
Rafael <rkyrdan@cogniance.com>
2013-10-11 14:36:52 +0300

    next step

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
 var http = require("http"),
     server = new http.Server(),
     url = require('url'),
-    log = require("./log")(module);
+    log = require("./log")(module),
+    fs = require("fs");
 
 
 server.on("request", function (req, res) {
+    var info;
 
-    var urlParsed = url.parse(req.url, true);
-    log.info("info");
-
-    if (req.method === "GET" && urlParsed.pathname === "/echo" && urlParsed.query.message) {
-        res.end(urlParsed.query.message);
-        log.debug("debug");
-        return;
+    if (req.url === "/") {
+        fs.readFile("index.html", function (err, info) {
+            if (err) {
+                res.statusCode = 500;
+                res.end("Server error");
+                return;
+            }
+            res.end(info);
+        });
+    } else if (req.url === "/now") {
+        res.end(new Date().toString());
     }
-
-    log.error("error");
-
-    res.statusCode = 404;
-    res.end("Page not found");
-
 });
 
 server.listen(1337, "127.0.0.1");
