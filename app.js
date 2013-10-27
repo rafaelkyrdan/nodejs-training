7e1907a3f5d9547e86acca63d89a006826139979
Rafael <rafaelkyrdan@gmail.com>
2013-10-27 18:07:53 +0200

    Middleware
    Middleware

--- app.js
+++ app.js
@@ -13,25 +13,47 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
+//app.use(express.cookieParser('your secret here'));
+//app.use(express.session());
+//app.use(app.router);
+//app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//if ('development' == app.get('env')) {
+//  app.use(express.errorHandler());
+//}
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/', routes.index);
+//app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+app.use(function (req, res, next) {
+
+    if (req.url === "/") {
+        res.send("the end");
+    } else if (req.url === "/forbidden") {
+        next(new Error("error happen"));
+    } else {
+        res.send(404, "not found");
+    }
+
+});
+
+app.use(function (err, req, res, next) {
+    if ('development' == app.get('env')) {
+        var errorHandler = express.errorHandler();
+        errorHandler(err, req, res, next);
+    } else {
+        res.send(500);
+    }
+});
+
