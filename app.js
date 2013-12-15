27a9858d1dd4201a742fc3ed8f6af2c8bb2c09e2
Rafael <rafaelkyrdan@gmail.com>
2013-12-15 18:06:10 +0200

    error handling
    error handling

--- app.js
+++ app.js
@@ -5,74 +5,46 @@ var express = require('express'),
     config = require("./conf"),
     log = require("./libs/log")(module),
     app = express(),
-    routes = require('./routes'),
-    user = require('./routes/user');
-
-// all environments
-//app.set('port', process.env.PORT || 3000);
+    HttpError = require('error').HttpError;
 
 app.engine('ejs', require('ejs-locals'));
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('views', path.join(__dirname, 'template'));
 app.set('views', __dirname + '/template');
 app.set('view engine', 'ejs');
+
+if (app.get('env') == 'development') {
+    app.use(express.logger('dev'));
+} else {
+    app.use(express.logger('default'));
+}
 
 app.use(express.favicon());
 app.use(express.logger({ immediate: true, format: 'dev' }));
 app.use(express.bodyParser());
-app.use(express.methodOverride());
-
-//app.use(express.cookieParser('your secret here'));
-//app.use(express.session());
-
+//app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(require('middleware/sendHttpError'));
 app.use(app.router);
+require('routes')(app);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-//if ('development' == app.get('env')) {
-//  app.use(express.errorHandler());
-//}
+app.use(function(err, req, res, next) {
+    if (typeof err == 'number') { // next(404);
+        err = new HttpError(err);
+    }
 
-//app.get('/', routes.index);
-//app.get('/users', user.list);
+    if (err instanceof HttpError) {
+        res.sendHttpError(err);
+    } else {
+        if (app.get('env') == 'development') {
+            express.errorHandler()(err, req, res, next);
+        } else {
+            log.error(err);
+            err = new HttpError(500);
+            res.sendHttpError(err);
+        }
+    }
+});
 
 http.createServer(app).listen(config.get('port'), function(){
-//  console.log('Express server listening on port ' + app.get('port'));
-  log.info('Express server listening on port ' + config.get('port'));
+    log.info('Express server listening on port ' + config.get('port'));
 });
-
-//app.get('/', function (req, res) {
-//    res.render("index", {
-//        title: "Rafael",
-//        body: "<b>Hello</b>"
-//    })
-//});
-
-app.get('/', function(req, res, next) {
-    res.render("index", {
-
-    });
-});
-
-
-//app.use(function (req, res, next) {
-//
-//    if (req.url === "/") {
-//        res.send("the end");
-//    } else if (req.url === "/forbidden") {
-//        next(new Error("error happen"));
-//    } else {
-//        res.send(404, "not found");
-//    }
-//
-//});
-//
-//app.use(function (err, req, res, next) {
-//    if ('development' == app.get('env')) {
-//        var errorHandler = express.errorHandler();
-//        errorHandler(err, req, res, next);
-//    } else {
-//        res.send(500);
-//    }
-//});
-
