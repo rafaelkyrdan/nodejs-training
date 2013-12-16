43e943b92d89b84f8ece42623f29370723576ab3
Rafael <rafaelkyrdan@gmail.com>
2013-12-16 21:59:49 +0200

    auth

--- models/user.js
+++ models/user.js
@@ -1,9 +1,10 @@
 var crypto = require('crypto'),
     mongoose = require('libs/mongoose'),
     Schema = mongoose.Schema,
-    schema;
+    util = require('util');
+    async = require('async');
 
-schema = new Schema({
+var schema = new Schema({
     username: {
         type: String,
         unique: true,
@@ -40,4 +41,44 @@ schema.methods.checkPassword = function(password) {
     return this.encryptPassword(password) === this.hashedPassword;
 };
 
+schema.statics.authorize = function(username, password, callback) {
+    var User = this;
+
+    async.waterfall([
+        function(callback) {
+            User.findOne({username: username}, callback);
+        },
+        function(user, callback) {
+            if (user) {
+                if (user.checkPassword(password)) {
+                    callback(null, user);
+                } else {
+                    callback(new AuthError("Пароль неверен"));
+                }
+            } else {
+                var user = new User({username: username, password: password});
+                user.save(function(err) {
+                    if (err) return callback(err);
+                    callback(null, user);
+                });
+            }
+        }
+    ], callback);
+};
+
 exports.User = mongoose.model('User', schema);
+
+
+function AuthError(message) {
+    Error.apply(this, arguments);
+    Error.captureStackTrace(this, AuthError);
+
+    this.message = message;
+}
+
+util.inherits(AuthError, Error);
+
+AuthError.prototype.name = 'AuthError';
+
+exports.AuthError = AuthError;
+
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,9 @@
-var User = require('models/user').User;
-    HttpError = require('error').HttpError;
-    ObjectID = require('mongodb').ObjectID;
+var checkAuth = require('middleware/checkAuth');
 
-module.exports = function(app) {
-
-    app.get('/', function(req, res, next) {
-        res.render("index");
-    });
-
-    app.get('/users', function(req, res, next) {
-        User.find({}, function(err, users) {
-            if (err) return next(err);
-            res.json(users);
-        })
-    });
-
-    app.get('/user/:id', function(req, res, next) {
-        try {
-            var id = new ObjectID(req.params.id);
-        } catch (e) {
-            next(404);
-            return;
-        }
-
-        User.findById(id, function(err, user) { // ObjectID
-            if (err) return next(err);
-            if (!user) {
-                return next(404);
-            }
-            res.json(user);
-        });
-    });
-
+module.exports = function (app) {
+    app.get('/', require('./frontpage').get);
+    app.get('/login', require('./login').get);
+    app.post('/login', require('./login').post);
+    app.post('/logout', require('./logout').post);
+    app.get('/chat', checkAuth, require('./chat').get);
 };
